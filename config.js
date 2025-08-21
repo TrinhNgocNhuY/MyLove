b52b74024cc5835dfaedf1b51debd6dc2dc5a2b6
TrinhNgocNhuY <phannhathung3101@gmail.com>
2025-08-21 19:32:29 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+    introTitle: 'Chị à!',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao em cứ đi nhầm đường
+    Lạc vào tim em lẻ loi
+    Đằng sau chữ yêu đây là thương`,
+    btnIntro: 'hihi',
+    title: 'chị ny à 😙',
+    desc: 'chị đồng ý làm ny em nha ',
+    btnYes: 'Chị đồng ý ❤️😘',
+    btnNo: 'Đéo :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao chị lại yêu em <3',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'Em biết mà 🥰. Yêu chị nhiều nhiều 😘😘',
+    messDesc: 'Tối nay mình nấc nhé.',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'http://fb.com/hungkphaihung'
+}
